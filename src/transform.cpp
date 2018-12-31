5d4cc79925b027553021708fbd9c9855b85a0714
Jeff Smith <ToxicGLaDOS@gmail.com>
2018-12-31 14:47:50 -0600

    Added a transform class. Have yet to integrate it.

---
+++ src/transform.cpp
@@ -0,0 +1,30 @@
+#include "transform.h"
+
+Transform::Transform(const Vector2& position, double rotation, const Vector2& scale)
+	: _position(position)
+	, _rotation(rotation)
+	, _scale(scale){}
+
+void Transform::setPosition(const Vector2& position){
+	_position = position;
+}
+
+void Transform::setRotation(double rotation){
+	_rotation = rotation;
+}
+
+void Transform::setScale(const Vector2& scale){
+	_scale = scale;
+}
+
+Vector2 Transform::position() const{
+	return _position;
+}
+
+double Transform::rotation() const{
+	return _rotation;
+}
+
+Vector2 Transform::scale() const{
+	return _scale;
+}
---
+++ src/transform.h
@@ -0,0 +1,27 @@
+#ifndef TRANSFORM_H
+#define TRNASFORM_H
+
+#include "vector2.h"
+
+class Transform {
+	public:
+		Transform(const Vector2& position, double rotation, const Vector2& scale);
+		
+		void setPosition(const Vector2& position);
+		void setRotation(double rotation);
+		void setScale(const Vector2& scale);
+
+		Vector2 position() const;
+		double rotation() const;
+		Vector2 scale() const;
+
+	protected:
+		Vector2 _position;
+		double _rotation;
+		Vector2 _scale;
+
+
+};
+
+
+#endif

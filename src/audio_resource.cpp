dbfc19ec8a182065cc7bdf357bd2221481da238c
Jeff Smith <ToxicGLaDOS@gmail.com>
2019-01-04 15:28:41 -0600

    Added some exceptions for files that aren't there.

--- src/audio_resource.cpp
+++ src/audio_resource.cpp
@@ -1,7 +1,7 @@
 #include "audio_resource.h"
 #include "exceptions.h"
 #include<allegro5/allegro_audio.h>
-
+#include<fstream>
 
 
 AudioResource::AudioResource(std::string path){
@@ -10,39 +10,47 @@ AudioResource::AudioResource(std::string path){
 	
 	ALLEGRO_PATH *tail = al_create_path(path.c_str());
 	al_join_paths(body, tail);
-		
-	_sample = al_load_sample(al_path_cstr(body, '/'));
-	if(_sample == NULL){
-		std::string err = std::string("Audio with path: ");
-		err.append(al_path_cstr(body, '/'));
-		err.append(" failed to load");
+	
+	const char* file_path = al_path_cstr(body, '/');
+
+	if(std::ifstream(file_path)){
+		_sample = al_load_sample(file_path);
+		if(_sample == NULL){
+			std::string err = std::string("Audio with path: ");
+			err.append(file_path);
+			err.append(" failed to load");
+			throw ResourceLoadException(err);
+		}
+		_voice = al_create_voice(_sampleRate, ALLEGRO_AUDIO_DEPTH_INT16, ALLEGRO_CHANNEL_CONF_2);
+		if(_voice == NULL){
+			throw AllegroCreationException("Allegro voice failed to be created");
+		}
+		_mixer = al_create_mixer(_sampleRate, ALLEGRO_AUDIO_DEPTH_FLOAT32, ALLEGRO_CHANNEL_CONF_2);
+		if(_mixer == NULL){
+			throw AllegroCreationException("Allegro mixer failed to be created");
+		}
+		if(!al_attach_mixer_to_voice(_mixer, _voice)){
+			throw AudioAttachException("Failed to attah mixer to voice");
+		}
+		_sampleInstance = al_create_sample_instance(NULL);
+		if(_sampleInstance == NULL){
+			throw AllegroCreationException("Allegro sample instance failed to be create");
+		}
+		if(!al_set_sample(_sampleInstance, _sample)){
+			// This might not be the most sensical exception to throw
+			// but I don't want to clutter things with too many kinds of exceptions
+			throw AudioAttachException("Failed to attach sample instance to sample");
+		}
+		if(!al_attach_sample_instance_to_mixer(_sampleInstance, _mixer)){
+			throw AudioAttachException("Failed to attach sample instance to mixer");
+		}
+		al_set_sample_instance_playmode(_sampleInstance, ALLEGRO_PLAYMODE_ONCE);
+	}
+	else{
+		std::string err = std::string("No file exists at path ");
+		err.append(file_path);
 		throw ResourceLoadException(err);
 	}
-	_voice = al_create_voice(_sampleRate, ALLEGRO_AUDIO_DEPTH_INT16, ALLEGRO_CHANNEL_CONF_2);
-	if(_voice == NULL){
-		throw AllegroCreationException("Allegro voice failed to be created");
-	}
-	_mixer = al_create_mixer(_sampleRate, ALLEGRO_AUDIO_DEPTH_FLOAT32, ALLEGRO_CHANNEL_CONF_2);
-	if(_mixer == NULL){
-		throw AllegroCreationException("Allegro mixer failed to be created");
-	}
-	if(!al_attach_mixer_to_voice(_mixer, _voice)){
-		throw AudioAttachException("Failed to attah mixer to voice");
-	}
-	_sampleInstance = al_create_sample_instance(NULL);
-	if(_sampleInstance == NULL){
-		throw AllegroCreationException("Allegro sample instance failed to be create");
-	}
-	if(!al_set_sample(_sampleInstance, _sample)){
-		// This might not be the most sensical exception to throw
-		// but I don't want to clutter things with too many kinds of exceptions
-		throw AudioAttachException("Failed to attach sample instance to sample");
-	}
-	if(!al_attach_sample_instance_to_mixer(_sampleInstance, _mixer)){
-		throw AudioAttachException("Failed to attach sample instance to mixer");
-	}
-	al_set_sample_instance_playmode(_sampleInstance, ALLEGRO_PLAYMODE_ONCE);
-	
 	
 	al_destroy_path(body);
 	al_destroy_path(tail);
--- src/image_resource.cpp
+++ src/image_resource.cpp
@@ -1,18 +1,31 @@
 #include"image_resource.h"
 #include"exceptions.h"
+#include<fstream>
 
 ImageResource::ImageResource(const std::string& path){
+	// TODO: Check if image addon has been initalized
+	// TODO: Add a check for the resources directory
 	ALLEGRO_PATH *body = al_get_standard_path(ALLEGRO_RESOURCES_PATH);
 	al_append_path_component(body, "resources");
 	
 	ALLEGRO_PATH *tail = al_create_path(path.c_str());
 	al_join_paths(body, tail);
+	
+	const char* file_path = al_path_cstr(body, '/');
 		
-	_bitmap = al_load_bitmap(al_path_cstr(body, '/'));
-	if(_bitmap == NULL){
-		std::string err = std::string("Image with path: ");
-		err.append(al_path_cstr(body, '/'));
-		err.append(" failed to load");
+	if(std::ifstream(file_path)){
+
+		_bitmap = al_load_bitmap(al_path_cstr(body, '/'));
+		if(_bitmap == NULL){
+			std::string err = std::string("Image with path: ");
+			err.append(file_path);
+			err.append(" failed to load");
+			throw ResourceLoadException(err);
+		}
+	}
+	else{
+		std::string err = std::string("No file exists at path: ");
+		err.append(file_path);
 		throw ResourceLoadException(err);
 	}
 	al_destroy_path(body);
